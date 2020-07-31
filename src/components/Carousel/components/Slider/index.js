a723966bafe5277d7cc9011f0597daf21ae96832
Angelica Zubieta <angelicazubieta@gmail.com>
2020-07-30 23:52:56 -0300

    estilos

--- src/components/Carousel/components/Slider/index.js
+++ src/components/Carousel/components/Slider/index.js
@@ -17,3 +17,27 @@ const Container = styled.ul`
             font-size: 30px; 
         } 
     }
+
+    .slick-prev {
+        left: 0;
+    }
+
+    .slick-next {
+        right: 16px;
+    }
+;
+
+export const SliderItem = styled.li
+    margin-right: 16px;
+    img{
+        margin: 16px;
+        width: 298px;
+        height: 197px;
+        object-fit: cover;
+    }
+;
+
+const Slider = ({ children }) => (
+    <Container>
+    lkmlkm
+)
